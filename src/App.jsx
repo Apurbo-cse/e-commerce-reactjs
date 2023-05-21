d942b849622648734dbc0f92600ca8e208815227
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-21 21:50:24 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
-import Category from "./components/home/category/Category";
 import SingleProduct from "./components/singleProduct/SingleProduct";
 import Footer from "./components/footer/Footer";
 import AppContext from "./utils/context";
+import Category from "./components/category/Category";
 
 function App() {
   return (
