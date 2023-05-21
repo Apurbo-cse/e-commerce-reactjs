4d9971e8e92d88a2054fb1616e4dfc24f6569c0e
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-21 11:12:03 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from "./components/home/Home";
 import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Category from "./components/home/category/Category";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/category/:id" element={<Category />} />
         </Routes>
       </BrowserRouter>
     </>
