75d8519fee034adc14b9017e4ae7cbadc0aeb14c
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-21 21:50:20 +0600

    Update Category.jsx

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import ProductCard from '../common/ProductCard'
 
 const Category = () => {
   return (
-    <div>Category</div>
+   <>
+   <div className="container">
+        <h5 className='p-2 pb-2 mb-0'>Category Title</h5>
+        <hr className='mt-0 '/>
+        <div className="row">
+           <ProductCard/>
+        </div>
+    </div>
+   </>
   )
 }
 
