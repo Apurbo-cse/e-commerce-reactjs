e55f233e248cfce80317f3196f15e8a586aebfc9
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-21 14:17:40 +0600

    Update

--- src/components/common/ProductCard.jsx
+++ src/components/common/ProductCard.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import cat from '../../assets/product.jpg'
+import cat from '../../assets/product.jpg';
+
 const ProductCard = () => {
   return (
     <>
       <div className="col-md-3 mb-2">
         <div className="card">
           <img src={cat} alt="" />
-          <p className=" px-2 m-0 text-muted">Lorem ipsum dolor sit amet.</p>
-          <h6 className=" px-2 text-muted">60TK</h6>
+          <p className="px-2 m-0 text-muted">Lorem ipsum dolor sit amet.</p>
+          <h6 className="px-2 text-muted">60TK</h6>
         </div>
       </div>
     </>
--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import cat from '../../assets/product.jpg'
+import ProductCard from '../common/ProductCard'
 const Product = () => {
   return (
     <>
@@ -7,13 +7,7 @@ const Product = () => {
         <h5 className='p-2 pb-2 mb-0'>Section Heading</h5>
         <hr className='mt-0 '/>
         <div className="row">
-            <div className="col-md-3 mb-2">
-                <div className="card">
-                    <img src={cat} alt="" />
-                    <p className=' px-2 m-0 text-muted'>Lorem ipsum dolor sit amet.</p>
-                    <h6 className=' px-2 text-muted'>60TK</h6>
-                </div>
-            </div>
+           <ProductCard/>
         </div>
     </div>
     </>
