098237e101d3824f74675d12601c1598f556beba
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-21 13:56:13 +0600

    Update Product.jsx

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Product = () => {
   return (
-    <div>Product</div>
+    <>
+    <div className="container">
+        <h5>Section Heading</h5>
+
+        <div className="row">
+            <div className="col-md-3 mb-2">
+                <div className="card">
+                    <img src={cat} alt="" />
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
   )
 }
 
