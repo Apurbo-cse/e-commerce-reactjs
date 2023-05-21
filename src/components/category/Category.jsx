22bdc7097d110b0a131c99b41a05c0e08238e38b
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-21 21:56:41 +0600

    Update Category.jsx

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -8,7 +8,7 @@ const Category = () => {
         <h5 className='p-2 pb-2 mb-0'>Category Title</h5>
         <hr className='mt-0 '/>
         <div className="row">
-           <ProductCard/>
+           <ProductCard innerPage/>
         </div>
     </div>
    </>
