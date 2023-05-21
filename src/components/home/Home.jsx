a2ea803564843b51aa86be989eac323284d36db2
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-21 14:05:15 +0600

    Update

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './banner/Banner'
 import Category from './category/Category'
+import Product from '../products/Product'
 
 const Home = () => {
   return (
     <>
       <Banner/>
       <Category/>
+      <Product/>
     </>
   )
 }
--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-
+import cat from '../../assets/product.jpg'
 const Product = () => {
   return (
     <>
     <div className="container">
-        <h5>Section Heading</h5>
-
+        <h5 className='p-2 pb-2 mb-0'>Section Heading</h5>
+        <hr className='mt-0 '/>
         <div className="row">
             <div className="col-md-3 mb-2">
                 <div className="card">
                     <img src={cat} alt="" />
-                    
+                    <p className=' px-2 m-0 text-muted'>Lorem ipsum dolor sit amet.</p>
+                    <h6 className=' px-2 text-muted'>60TK</h6>
                 </div>
             </div>
         </div>
