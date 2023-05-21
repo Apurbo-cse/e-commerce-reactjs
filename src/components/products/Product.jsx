7bf75fc3256bb58c30360b4d1f92b63595d32c6d
Apurbo Kumar Anup <apurboka@gmail.com>
2023-05-21 21:50:14 +0600

    Update Product.jsx

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ProductCard from '../common/ProductCard'
-const Product = () => {
+const Product = ({innerPage}) => {
   return (
     <>
     <div className="container">
-        <h5 className='p-2 pb-2 mb-0'>Section Heading</h5>
+       {innerPage && <h5 className='p-2 pb-2 mb-0'>Section Heading</h5> }
         <hr className='mt-0 '/>
         <div className="row">
            <ProductCard/>
